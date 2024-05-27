ffd343320fd25ac10b6b7fb98e44047d5242eea7
Oluwasetemi Ojo <setemiojo@gmail.com>
2024-05-26 23:24:04 -0500

    chore: Update GitHub icon import in global-bottom.vue

--- global-bottom.vue
+++ global-bottom.vue
@@ -1,15 +1,15 @@
 <!-- global-bottom.vue -->
 <script setup>
-import IconAccountBox from '~icons/mdi/github';
+import GitHub from '~icons/mdi/github';
 </script>
 <template>
-  <footer class="absolute bottom-0 right-0 p-2 mb-9 mr-9">
+  <footer class="abs-tr p-2 mb-9 mr-9 hover:underline hover:underline-offset-8 focus:ring active:text-red-500">
     <a
       href="https://github.com/users/Oluwasetemi"
       target="_blank"
       rel="noopener noreferrer"
     >
-      <icon-account-box style="font-size: 1em" />
+      <GitHub style="font-size: 1em" />
       @Oluwasetemi
     </a>
   </footer>
