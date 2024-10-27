12cdcfc37ea34959878131216fc2090c004b8468
oluwasetemi ojo <setemiojo@gmail.com>
2024-10-26 22:15:10 -0500

    use iframe, move to slidev default theme, vite and unocss configs

--- components/CodepenBreadcrumbs.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <iframe height="500" style="width: 100%;" scrolling="no" title="breadcrumbs and skip-navigation" src="https://codepen.io/setemiojo/embed/JjxNJVm?default-tab=html%2Cresult" frameborder="no" loading="lazy" allowtransparency="true" allowfullscreen="true">
-  See the Pen <a href="https://codepen.io/setemiojo/pen/JjxNJVm">
-  breadcrumbs and skip-navigation</a> by Ojo Oluwasetemi Stephen (<a href="https://codepen.io/setemiojo">@setemiojo</a>)
-  on <a href="https://codepen.io">CodePen</a>.
-  </iframe>
-</template>
